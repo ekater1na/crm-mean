afa55544964a21f81339b01857d169ffc5cc59b1
ekater1na <ekaterina_l@tut.by>
2020-11-29 01:33:14 +0300

    feat: add registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,36 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
 module.exports.login = function(req, res) {
-    res.status(200).json({
-        login: {
-            email: req.body.email,
-            password: req.body.password        
-        }
-    })    
+   
 }
 
-module.exports.register = function(req, res) {
-    res.status(200).json({
-        register: 'from controller'
-    })
+module.exports.register = async function(req, res) {
+    // email, password
+    const candidate = await User.findOne({email: req.body.email})
+
+    if (candidate) {
+        //user exist, return error
+        res.status(409).json({
+            message: 'That email is taken. Try another'
+        })
+    } else {
+        //user will be created
+        const salt = bcrypt.genSaltSync(10)
+        const password = req.body.password
+        const user  = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password, salt)
+        })
+
+        try {
+            await user.save()
+            res.status(201).json(user)
+        } catch(e) {
+            // catch error
+
+        }
+        
+
+    }
 }
