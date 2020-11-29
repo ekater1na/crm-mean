867b5f2f6d7245f9aeb2a40650ff8a361a8ecf13
ekater1na <ekaterina_l@tut.by>
2020-11-29 23:09:14 +0300

    feat: add authorisation with jwt-token

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURL: 'mongodb://katia:123456k@cluster0-shard-00-00.3zbdu.mongodb.net:27017,cluster0-shard-00-01.3zbdu.mongodb.net:27017,cluster0-shard-00-02.3zbdu.mongodb.net:27017/test?replicaSet=atlas-bx05rb-shard-0&ssl=true&authSource=admin'
+    mongoURL: 'mongodb://katia:123456k@cluster0-shard-00-00.3zbdu.mongodb.net:27017,cluster0-shard-00-01.3zbdu.mongodb.net:27017,cluster0-shard-00-02.3zbdu.mongodb.net:27017/test?replicaSet=atlas-bx05rb-shard-0&ssl=true&authSource=admin',
+    jwt: 'dev-jwt'
 }
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,34 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../config/keys');
 const User = require('../models/User');
 
-module.exports.login = function(req, res) {
-   
+module.exports.login = async function(req, res) {
+    const candidate = await User.findOne({email: req.body.email})
+    if (candidate) {
+        // check password, user exist
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password)
+        if (passwordResult) {
+            // token generation, equal passwords
+            const token = jwt.sign({
+                email: candidate.email,
+                userid: candidate._id
+            }, keys.jwt, {expiresIn: 60 * 60});
+            res.status(200).json({
+                token: `Bearer ${token}`
+            })
+        } else {
+            res.status(401).json({
+                // passwords don't match
+                message: 'Passwords don\'t match. Try again'
+            })
+        }
+    } else {
+        // user doesn't exist, error
+        res.status(404).json({
+            message: 'User with that email not found'
+        })
+    }
 }
 
 module.exports.register = async function(req, res) {
@@ -10,12 +36,12 @@ module.exports.register = async function(req, res) {
     const candidate = await User.findOne({email: req.body.email})
 
     if (candidate) {
-        //user exist, return error
+        // user exist, return error
         res.status(409).json({
             message: 'That email is taken. Try another'
         })
     } else {
-        //user will be created
+        // user will be created
         const salt = bcrypt.genSaltSync(10)
         const password = req.body.password
         const user  = new User({
