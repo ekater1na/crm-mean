12959e47bac99401c054f99b8cea91974652d60f
ekater1na <ekaterina_l@tut.by>
2020-12-01 01:12:46 +0300

    feat: add authorisation with passportJs

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 const bodyPareser = require('body-parser');
 const authRoutes = require('./routes/auth');
 const analyticsRoutes = require('./routes/analytics');
@@ -11,10 +12,14 @@ const app = express();
 
 mongoose.connect(keys.mongoURL, {
     useNewUrlParser: true,
+    useCreateIndex: true,
     useUnifiedTopology: true
   })
     .then(() => console.log('MongoDB connected'))
     .catch(error => console.log(error))
+
+app.use(passport.initialize())
+require('./middleware/passport')(passport)
 
 app.use(require('morgan')('dev'));
 app.use(bodyPareser.urlencoded({extended: true}));
